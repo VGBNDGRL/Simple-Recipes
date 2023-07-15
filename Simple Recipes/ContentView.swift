fb2df9efb646f1363cd336e71285c33c09bde1ac
Anna Flores <annaflowersk@gmail.com>
2023-07-15 13:17:32 -0500

    Initial Commit

---
+++ Simple Recipes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Simple Recipes
+//
+//  Created by Anna Flores on 7/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Recipes/Simple_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_RecipesApp.swift
+//  Simple Recipes
+//
+//  Created by Anna Flores on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
